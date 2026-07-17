f57b98909f1556ab1a897f957407e3cb4ff180ae
ps-jobayar <psjobayar@gmail.com>
2026-07-17 12:42:17 +0600

    Upload src/context/SettingsContext.tsx

---
+++ src/context/SettingsContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const SettingsContext = createContext<any>(null);
+
+export const SettingsProvider = ({ children }: { children: React.ReactNode }) => {
+  const [panelName, setPanelName] = useState<string>("ZIBON Panel");
+  const [panelLogo, setPanelLogo] = useState<string>("");
+  const [panelBackgroundImage, setPanelBackgroundImage] = useState<string>("");
+  const [panelBackgroundBlur, setPanelBackgroundBlur] = useState<number>(10);
+  const [enablePlayit, setEnablePlayit] = useState<boolean>(false);
+
+  const fetchSettings = async () => {
+    try {
+      const res = await axios.get("/api/settings");
+      if (res.data.panelName) {
+        setPanelName(res.data.panelName);
+      }
+      if (res.data.panelLogo !== undefined) {
+        setPanelLogo(res.data.panelLogo);
+      }
+      if (res.data.panelBackgroundImage !== undefined) {
+        setPanelBackgroundImage(res.data.panelBackgroundImage);
+      }
+      if (res.data.panelBackgroundBlur !== undefined) {
+        setPanelBackgroundBlur(res.data.panelBackgroundBlur);
+      }
+      if (res.data.enablePlayit !== undefined) {
+        setEnablePlayit(res.data.enablePlayit);
+      }
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    fetchSettings();
+  }, []);
+
+  return (
+    <SettingsContext.Provider value={{ panelName, setPanelName, panelLogo, setPanelLogo, panelBackgroundImage, setPanelBackgroundImage, panelBackgroundBlur, setPanelBackgroundBlur, enablePlayit, setEnablePlayit, fetchSettings }}>
+      {children}
+    </SettingsContext.Provider>
+  );
+};
+
+export const useSettings = () => useContext(SettingsContext);
